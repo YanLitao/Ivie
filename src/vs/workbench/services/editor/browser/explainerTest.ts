48e6ffcf3f2243084897729413dab0bfdc1b6d25
YanLitao <yanlitao@outlook.com>
2023-01-30 12:49:50 -0500

    add copilot and text diff function

--- src/vs/workbench/services/editor/browser/explainerTest.ts
+++ src/vs/workbench/services/editor/browser/explainerTest.ts
@@ -6,12 +6,33 @@ export function addExplainer() {
 	console.log("added explainer");
 }
 
+function diffText(textA: string, textB: string) {
+	var diff = "";
+	var text1 = textA.split("\n"),
+		text2 = textB.split("\n"),
+		startIdx = 0;
+	for (var i = 0; i < text1.length; i++) {
+		var flag = false;
+		for (var j = startIdx; j < text2.length; j++) {
+			if (text1[i] === text2[j]) {
+				flag = true;
+				startIdx = j + 1;
+				break;
+			}
+		}
+		if (flag === false) {
+			diff += text1[i] + "\n";
+		}
+	}
+	return diff;
+}
+
 export class Explainer {
 	public static readonly ID = 'editor.contrib.explainer';
 	constructor(
 		private readonly _editor: ICodeEditor,
 		private _box: HTMLDivElement,
-		//private _posttext: string,
+		private _posttext: string,
 		private _pretext: string
 	) {
 		this._editor.onKeyDown((e) => { this.onKeyDown(e); });
@@ -20,39 +41,36 @@ export class Explainer {
 	}
 
 	private onKeyDown(e: IKeyboardEvent) {
-		console.log(this._editor);
-		const editor_div = this._editor.getDomNode();
-		if (editor_div === null) {
-			throw new Error('Cannot find Monaco Editor');
-		}
-		if (document.getElementById("explainer_container") !== null) {
-			return;
-		}
-		this._box = document.createElement('div');
-		this._box.style.position = 'absolute';
-		this._box.style.top = '0px'; // offset from the run button + border + padding
-		this._box.style.bottom = '14px'; // offset from the horizontal scroll bar (if any)
-		this._box.style.right = '200px';
-		this._box.style.height = 'auto';
-		this._box.style.width = '500px';
-		this._box.style.backgroundColor = 'rgba(10, 10, 10, 0.2)';
-		this._box.innerText = 'Hello World';
-		this._box.id = "explainer_container";
-		editor_div.appendChild(this._box);
-		//editor_div.getElementsByClassName("lines-content monaco-editor-background")
+		this._pretext = this._editor.getValue();
 	}
 
 	private onKeyUp(e: IKeyboardEvent) {
-		var model = this._editor.getModel();
-		if (model === null) {
-			throw new Error('Cannot find Monaco Editor');
+		this._posttext = this._editor.getValue();
+		var diffed = diffText(this._posttext, this._pretext);
+		console.log(diffed);
+		if (diffed.split("\n").length > 3) {
+			const editor_div = this._editor.getDomNode();
+			if (editor_div === null) {
+				throw new Error('Cannot find Monaco Editor');
+			}
+			if (document.getElementById("explainer_container") !== null) {
+				return;
+			}
+			this._box = document.createElement('div');
+			this._box.style.position = 'absolute';
+			this._box.style.top = '0px'; // offset from the run button + border + padding
+			this._box.style.bottom = '14px'; // offset from the horizontal scroll bar (if any)
+			this._box.style.right = '200px';
+			this._box.style.height = 'auto';
+			this._box.style.width = '500px';
+			this._box.style.backgroundColor = 'rgba(10, 10, 10, 0.2)';
+			this._box.innerText = 'Hello World';
+			this._box.id = "explainer_container";
+			this._box.style.zIndex = '100';
+			var parent = editor_div.getElementsByClassName("lines-content monaco-editor-background")
+			parent[0].appendChild(this._box);
+			console.log(parent);
 		}
-		var sel = this._editor.getSelection();
-		if (sel === null) {
-			throw new Error('Cannot find Monaco Editor');
-		}
-		this._pretext = model.getValueInRange(sel);
-		console.log(this._pretext);
 	}
 
 	public dispose(): void {
